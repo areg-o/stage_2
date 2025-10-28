d32995230010c6e9f06060c8c085531c280156c5
Areg <foritscope@gmail.com>
2025-10-28 23:48:50 +0400

    feat: add menu bar

---
+++ src/components/Logo.tsx
@@ -0,0 +1,8 @@
+export function Logo() {
+  return (
+    <div className="flex flex-row justify-center items-center w-[7.7vw]">
+      <img className="w-[2.9vw] h-[2.9vw]" src="logo.png" alt="Logo" />
+      <span className="ml-[1vw]">Base</span>
+    </div>
+  );
+}
---
+++ src/components/Menu.tsx
@@ -0,0 +1,8 @@
+export function Menu() {
+  return (
+    <div className="flex flex-col justify-center mt-[5vh]">
+      <span className="cursor-pointer text-[#605BFF] hover:text-black">Dashboard</span>
+      <span className="cursor-pointer text-[#605BFF] hover:text-black">Add</span>
+    </div>
+  );
+}
